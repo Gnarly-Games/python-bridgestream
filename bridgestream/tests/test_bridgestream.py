a6ce07b6e363de8495fe5ef5626ebc6df49d2350
mgurdal <mgurdal@pm.me>
2020-10-31 04:47:56 +0300

    Optimize import

--- bridgestream/tests/test_bridgestream.py
+++ bridgestream/tests/test_bridgestream.py
@@ -1,6 +1,6 @@
 import unittest
 
-from bridgestream.bridgestream import BridgeStream, BridgeSerializer
+from bridgestream import BridgeStream, BridgeSerializer
 
 
 class MatchInfo(BridgeSerializer):
